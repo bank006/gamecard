cd1d496013c0afd44bae1e3e40ac3ef93aa69806
bank006 <88577275+bank006@users.noreply.github.com>
2024-08-11 18:27:00 +0700

    Add

--- app.py
+++ app.py
@@ -978,3 +978,6 @@ def handle_update_score(data):
 # if __name__ == '__main__':
     # socketio.run(app, debug=True, use_reloader=False)
 
+# if __name__ == '__main__':
+#     socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+
